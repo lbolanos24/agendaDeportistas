f12f38400023be5749b979176fb70a40d1309ba6
lpbolanos24 <lpbolanos@hotmail.com>
2024-08-01 08:44:01 -0500

    Formulario depottista

--- src/components/Deportistas/EditarDeportistas.tsx
+++ src/components/Deportistas/EditarDeportistas.tsx
@@ -173,7 +173,7 @@ function EditarDeportistas(props: Props) {
         </GridItem>
       </Grid>
       <Grid 
-      h="500px"
+      h="900px"
       templateRows="repeat(4, 1fr)"
       templateColumns="repeat(1, 1fr)"
       gap={1}
@@ -200,35 +200,34 @@ function EditarDeportistas(props: Props) {
           </GridItem>
           <GridItem rowSpan={1} colSpan={1}>
             <FormControl isRequired>
-            <FormLabel>Información de mensualidad</FormLabel>
-              XXXX terminos de aceptacion de la informacion de mensualidad del deportista XXXX
-            <Input type="checkbox"
+            <FormLabel>El pago de las mensualidades corresponden al trabajo de 4 semanas por mes y de ahí se despliega la cantidad de clases que adquiera la familia por semana, si por algún motivo nos vemos en la necesidad de cancelar alguna de las clases (Festivo, eventos deportivos, incapacidad del profesor sin posible reemplazo) se tendrá en cuenta le número de clases dictadas al grupo para hacer la reposición de la misma. En cuyo caso, será el profesor titular del grupo quien coordinará con las familias la reposición de la clase procurando que la mayoría de los integrantes del grupo pueda asistir en un mismo horario, de no ser posible le pedimos por favor nos envíe un correo con el asunto SOLICITUD DE REPOSICIÓN y el número de documento del niño o la niña, así podremos desde nuestra base de datos indicarle cuales son las opciones para dicha reposición. La respuesta a este punto es ENTENDIDO</FormLabel>
+             <Input type="checkbox"
               onChange={(e) => setInformacionMensualidad(e.target.value)}
             />
           </FormControl>
           </GridItem>
           <GridItem rowSpan={1} colSpan={1}>
             <FormControl isRequired>
-            <FormLabel>Información de Repsición</FormLabel>
-              XXXX terminos de aceptacion de la informacion de reposicion del deportista XXXX
-            <Input type="checkbox"
+            <FormLabel>Entendemos que en algunos casos se pueden presentar inasistencias, es importante para nosotros que usted como adulto tenga claro que para realizar una reposición de clase necesitamos que nos comparta un comprobante medico para reponer todas las clases que sean necesarias debido a dicha incapacidad, y en caso de no tenerlo solo podremos reponer 1 clase por mes con previa reserva. 
+            Para esto deberá enviarnos un correo con el asunto SOLICITUD DE REPOSICIÓN y el número de documento del niño o la niña, así podremos desde nuestra base de datos indicarle cuales son las opciones para dicha reposición. La respuesta a este punto es ENTENDIDO</FormLabel>
+              <Input type="checkbox"
               onChange={(e) => setInformacionReposicion(e.target.value)}
             />
           </FormControl>
           </GridItem>
           <GridItem rowSpan={1} colSpan={1}>
             <FormControl isRequired>
-            <FormLabel>Información de Vacaciones</FormLabel>
-              XXXX terminos de aceptacion de la informacion de vacaciones del deportista XXXX
-            <Input type="checkbox"
+            <FormLabel>Con respecto a las fechas de vacaciones (inicio de año, semana santa, vacaciones de mitad de año, semana de octubre y final de año) consideramos importante  que las familias sepan que no serán contempladas en los cobros de mensualidades, y por tanto no se dictarán para respetar los planes familiares. 
+            Para las familias que prefieren conservar las actividades se realizarán propuesta de intensivos para estas fechas y de acuerdo a la cobertura se extenderán la mayor cantidad de tiempo posible y para organizar estas actividades será el profesor titular del grupo quien coordinará con las familias este trabajo, si por algún motivo el profesor no dictará estas actividades será él quien direccione a las familias interesadas con el área administrativa para ayudarlas con su necesidad. La respuesta a este punto es ENTENDIDO</FormLabel>
+             <Input type="checkbox"
               onChange={(e) => setInformacionVacaciones(e.target.value)}
             />
             </FormControl>
           </GridItem>
           <GridItem rowSpan={1} colSpan={1}>
             <FormControl isRequired>
-              <FormLabel>Comprobante de Inscripción</FormLabel>
-              XXXX Registro del comprobante de inscripcion deportista XXXX
+              <FormLabel>Para finalizar el proceso de inscripción y entendiendo que está de acuerdo con lo descrito en este formulario. Le pedimos por favor cancelar el valor de la matrícula por $100.000COP a la cuenta de ahorros 00682228130 de Bancolombia y nos comparta el comprobante.
+              La matrícula corresponde al seguro contra accidentes deportivos con cobertura desde el 1 de enero hasta el 31 de diciembre del presente año y esta debe renovarse cada año.</FormLabel>
               <Input type="checkbox"
               onChange={(e) => setComprobanteInscripcion(e.target.value)}
             />
@@ -237,7 +236,7 @@ function EditarDeportistas(props: Props) {
     </Grid>
 
       <Grid 
-      h="300px"
+      h="150px"
       templateRows="repeat(2, 1fr)"
       templateColumns="repeat(2, 1fr)"
       gap={2}
